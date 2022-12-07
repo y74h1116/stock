30eb8efd6a7b895bc670183dcca330633ba2026f
y.h <y.h@local>
2022-12-07 11:43:20 +0900

    feat: 入門 Node.js プログラミング LESSON 7 リスト 7-5 の写経

---
+++ node/zzz_first/lesson007/confetti_cuisine/utils.js
@@ -0,0 +1,19 @@
+// getFile で使うモジュールをインポート
+const fs = require("fs");
+const { METHOD_FAILURE } = require("http-status-codes");
+const httpStatus = require("http-status-codes");
+const contentTypes = require("./contentType");
+
+// ファイルを読み込んでレスポンスを返す getFile 関数をエクスポート
+module.exports = {
+    getFile: (file, res) => {
+        fs.readFile(`./${file}`, (error, data) => {
+            if (error) {
+                res.writeHead(httpStatus.INTERNAL_SERVER_ERROR, contentTypes.html);
+                res.end("There was an error serving content!");
+                // 書籍のサンプルでは、特に return していないが、ここで return した方がよさおうな気がする
+            }
+            res.end(data);
+        });
+    }
+};
