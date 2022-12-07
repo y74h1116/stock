80d801149b4f26bb1f2378de8f62673fbfe001ac
y.h <y.h@local>
2022-12-07 11:33:45 +0900

    feat: 入門 Node.js プログラミング LESSON 7 リスト 7-4 の写経

---
+++ node/zzz_first/lesson007/confetti_cuisine/contentTypes.js
@@ -0,0 +1,9 @@
+// コンテンツタイプをマップするオブジェクトをエクスポート
+module.exports = {
+    html:   {"Content-Type": "text/html"},
+    text:   {"Content-Type": "text/text"},
+    js:     {"Content-Type": "text/js"},
+    jpg:    {"Content-Type": "image/jpg"},
+    png:    {"Content-Type": "image/png"},
+    css:    {"Content-Type": "text/css"},
+};
