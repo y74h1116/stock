eb599e8b513705d30399f17891a001c3bdcd48a2
y.h <y.h@local>
2022-12-08 15:28:46 +0900

    feat: 入門 Node.js プログラミング LESSON 7 リスト 7-6 の写経

---
+++ node/zzz_first/lesson007/confetti_cuisine/router.js
@@ -0,0 +1,30 @@
+const httpStatus = require("http-status-codes");
+const contentTypes = require("./contentTypes");
+const utils = require("./utils");
+
+// 経路の関数を入れる routes オブジェクト
+const routes = {
+    "GET": {},
+    "POST": {},
+};
+
+// リクエストを処理する handle 関数
+exports.handle = (req, res) => {
+    try {
+        routes[req.method][req.utl](req.res);
+    } catch (e) {
+        res.writeHead(httpStatus.OK, contentTypes.html);
+        utils.getFile("views/error.html", res);
+    }
+};
+
+// GET メソッドの経路関数をマップする関数
+exports.get = (url, action) = {
+    routes["GET"][url] = action;
+};
+
+// POST メソッドの経路関数をマップする関数
+exports.post = (url, action) = {
+    routes["POST"][url] = action;
+};
+
