c483f00efd3b21174aa965b1271c375b9dcbbe00
y.h <y.h@local>
2022-12-12 14:43:48 +0900

    feat: Go 言語 チュートリアル 間違えて小文字にしていた→大文字に修正修正

--- go/zzz_first/greetings_module/greetings.go
+++ go/zzz_first/greetings_module/greetings.go
@@ -3,7 +3,8 @@ package greetings
 import "fmt"
 
 // Hello returns a greeting for the named person
-func hello(name string) string {
+// ※先頭は大文字にすること (間違えて、小文字にしていたら、どうしても動かなかった)
+func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	// := は、宣言と初期化を同時にする省略形みたい (コンパイラが推論できるように初期化するみたい)
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
--- go/zzz_first/greetings_module/readme.md
+++ go/zzz_first/greetings_module/readme.md
@@ -1,1 +1,6 @@
 - `Tutorial: Create a Go module`の写経  
+- 間違えて、`func Hello`を小文字`func hello`にしていたら、どうしても動かなかったので注意！  
+    - 参照 : https://go.dev/tour/basics/3  
+        - In Go, a name is exported if it begins with a capital letter.   
+    - どうやら、エクスポートするものは大文字から始める必要があるようだ  
+
