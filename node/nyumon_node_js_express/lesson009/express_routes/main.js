abb274747a13841ff5f721377f20391c7e87a9f4
y.h <y.h@local>
2022-12-13 13:38:24 +0900

    feat: 入門 Node.js プログラミング LESSON 9 リスト 9-3 の写経

---
+++ node/nyumon_node_js_express/lesson009/express_routes/main.js
@@ -0,0 +1,19 @@
+const port = 8888;
+const express = require("express");
+
+const app = express();
+
+app.get("/items/:vegetable", (req, res) => {
+    console.log(req.params);
+    console.log(req.body);  // GET メソッドでも、body は何か入ってくるのかな？
+    console.log(req.url);
+    console.log(req.query);
+    // サーバーからクライアントへのレスポンスを発行
+    let veg = req.params.vegetable;
+    res.send(`This is the page for ${veg}`);
+})
+
+app.listen(port, () =>{
+    console.log(`The Express.js server has started and is listening on port number:` + `${port}`);
+});
+
