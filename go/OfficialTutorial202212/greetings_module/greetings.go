30048ebca9e612b246a69ecea62b4ad2b71c517c
y.h <y.h@local>
2022-12-13 11:32:32 +0900

    feat: Go 言語 チュートリアル Return and handle an error の greetings.go の写経

--- go/OfficialTutorial202212/greetings_module/greetings.go
+++ go/OfficialTutorial202212/greetings_module/greetings.go
@@ -1,13 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person
 // ※先頭は大文字にすること (間違えて、小文字にしていたら、どうしても動かなかった)
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
+func Hello(name string) (string, error) {
+	// IF no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// := は、宣言と初期化を同時にする省略形みたい (コンパイラが推論できるように初期化するみたい)
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	// Return a greeting that embeds the name in a message.
+	return message, nil
 }
 
