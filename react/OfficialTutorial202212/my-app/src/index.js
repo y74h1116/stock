dd1e768644a0d32731c1a4376d8bd653997fc091
y.h <y.h@local>
2022-12-13 09:59:51 +0900

    feat: React チュートリアルの写経 Game コンポーネントの render 関数を修正

--- react/OfficialTutorial202212/my-app/src/index.js
+++ react/OfficialTutorial202212/my-app/src/index.js
@@ -106,16 +106,29 @@ class Game extends React.Component {
     }
 
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+        }
+
         return (
-        <div className="game">
-            <div className="game-board">
-            <Board />
+            <div className="game">
+                <div className="game-board">
+                <Board 
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}
+                />
+                </div>
+                <div className="game-info">
+                <div>{status}</div>
+                <ol>{/* TODO */}</ol>
+                </div>
             </div>
-            <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-            </div>
-        </div>
         );
     }
 }
