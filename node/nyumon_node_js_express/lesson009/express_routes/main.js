39503418af90be24ea3723d501d89a814914ab7e
y.h <y.h@local>
2022-12-13 15:15:15 +0900

    feat: 入門 Node.js プログラミング LESSON 9 リスト 9-5 の写経

--- node/nyumon_node_js_express/lesson009/express_routes/main.js
+++ node/nyumon_node_js_express/lesson009/express_routes/main.js
@@ -3,11 +3,36 @@ const express = require("express");
 
 const app = express();
 
+// URLエンコードされたデータを解析する
+app.use(
+    // 参照 : https://expressjs.com/ja/api.html#express.urlencoded
+    //   It parses incoming requests with urlencoded payloads
+    //   URLエンコードされたペイロードをパースしてくれるということか
+    express.urlencoded({
+        extended: false
+    })
+);
+
+// 参照 : https://expressjs.com/ja/api.html#express.json
+//   It parses incoming requests with JSON payloads
+//   この例では、application/x-www-form-urlencoded でポストしているから、express.json() は不要な気がする
+app.use(express.json());
+
 // ミドルウェア関数を定義
 app.use((req, res, next) => {
     // リクエストのパスをログに出す
     console.log(`request made to: ${req.url}`);
     next(); // next関数を呼び出さないとリクエストがハングする
+});
+
+// ホームページ用のPOST 経路を定義
+// ※curl コマンド例
+//     - (例 1) curl -s -v -X POST http://localhost:8888/  -d "first_name=Jon&last_name=Wexler"
+//     - (例 2 URLエンコードしている例) curl -s -v -X POST http://localhost:8888/  -d "aaaa=a+a+a"
+app.post("/", (req, res) => {
+    console.log(req.body);  // リクエスト本文をロギング
+    console.log(req.query);
+    res.send("POST Successful!");
 });
 
 app.get("/items/:vegetable", (req, res) => {
