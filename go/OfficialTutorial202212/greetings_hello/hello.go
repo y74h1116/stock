334ca427c8b632843d2e903e5b13d95e73d41809
y.h <y.h@local>
2022-12-13 11:33:21 +0900

    feat: Go 言語 チュートリアル Return and handle an error の hello.go の写経

--- go/OfficialTutorial202212/greetings_hello/hello.go
+++ go/OfficialTutorial202212/greetings_hello/hello.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"example.com/greetings"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	// 
+	// ※log パッケージのドキュメントは下記
+	//   https://pkg.go.dev/log
+	//   https://pkg.go.dev/log#SetPrefix
+	//   https://pkg.go.dev/log#SetFlags
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeeting message.
+	// このチュートリアルは、エラーハンドリングがテーマなので
+	// 意図的にエラーになるパラメータを渡す
+	message, err := greetings.Hello("")
+	// If an error as returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
 	fmt.Println(message)
 }
