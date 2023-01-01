214ff124040c4b72a9bc758cbd7fc2b3a6cfa936
y.h <y.h@local>
2023-01-02 01:44:12 +0900

    feat: 入門 Node.js プログラミング LESSON 10 リスト10-2：コントローラ homeController の写経

---
+++ node/nyumon_node_js_express/lesson010/express_templates/controllers/homeController.js
@@ -0,0 +1,7 @@
+exports.respondWithName = (req, res) => {
+    // カスタム EJS ビューで応答する
+    res.render("index");
+        // ※書籍のメモ
+        // index.ejs を指定する際、拡張子(.ejs)の指定は不要
+        //　ビューのパスも指定不要
+};
