d74cbb2eb1f5540f006f674d7bd3169f325c3d35
y.h <y.h@local>
2023-01-02 00:38:51 +0900

    feat: Go 言語 チュートリアル Return greetings for multiple people の写経 (greetings.go)

--- go/OfficialTutorial202212/greetings_module/greetings.go
+++ go/OfficialTutorial202212/greetings_module/greetings.go
@@ -20,6 +20,25 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellows returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// It the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 // init sets initial values for variables used in the function.
 // ※参照：https://tutorialedge.net/golang/the-go-init-function/
 //       ↑のリンクに init 関数が、main 関数より先に呼ばれることを検証するテストプログラムが掲載されているけど
